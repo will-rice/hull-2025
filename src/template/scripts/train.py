a5235491e6803a017e06187ef1718fe6cdf1b4a5
Will Rice <will@worbler.ai>
2025-10-15 07:43:19 -0400

    Initial commit

---
+++ src/template/scripts/train.py
@@ -0,0 +1,9 @@
+"""Train script."""
+
+def main() -> None:
+    """Train script."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_dummy.py
@@ -0,0 +1,5 @@
+"""Dummy test."""
+
+def test_dummy() -> None:
+    """Dummy test."""
+    assert True
